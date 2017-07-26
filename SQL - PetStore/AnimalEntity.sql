3ad00077e74c9155e1a6e53e29c7953549489f69
eaperador <e.aperador@uniandes.edu.co>
2017-07-26 11:09:45 -0500

    agregar consultas sql

---
+++ SQL - PetStore/AnimalEntity.sql
@@ -0,0 +1,47 @@
+insert into ANIMALENTITY (id, birthdate, color, gender, name, breed_id) values (1, '10/16/2015', 'Puce', 'Male', 'Weylin', 1);
+insert into ANIMALENTITY (id, birthdate, color, gender, name, breed_id) values (2, '6/18/2015', 'Pink', 'Female', 'Joli', 1);
+insert into ANIMALENTITY (id, birthdate, color, gender, name, breed_id) values (3, '7/19/2017', 'Turquoise', 'Male', 'Keelby', 1);
+
+insert into ANIMALENTITY (id, birthdate, color, gender, name, breed_id) values (4, '4/4/2016', 'Purple', 'Female', 'Estrella', 2);
+insert into ANIMALENTITY (id, birthdate, color, gender, name, breed_id) values (5, '3/8/2016', 'Teal', 'Male', 'Saundra', 2);
+insert into ANIMALENTITY (id, birthdate, color, gender, name, breed_id) values (6, '3/5/2017', 'Green', 'Male', 'Wadsworth', 2);
+
+insert into ANIMALENTITY (id, birthdate, color, gender, name, breed_id) values (7, '8/3/2015', 'Puce', 'Female', 'Samaria', 3);
+insert into ANIMALENTITY (id, birthdate, color, gender, name, breed_id) values (8, '8/3/2016', 'Turquoise', 'Male', 'Bartholomeus', 3);
+insert into ANIMALENTITY (id, birthdate, color, gender, name, breed_id) values (9, '5/21/2017', 'Violet', 'Male', 'Hilario', 3);
+
+insert into ANIMALENTITY (id, birthdate, color, gender, name, breed_id) values (10, '4/23/2017', 'Crimson', 'Male', 'Conan', 4);
+insert into ANIMALENTITY (id, birthdate, color, gender, name, breed_id) values (11, '5/14/2017', 'Yellow', 'Female', 'Madelin', 4);
+insert into ANIMALENTITY (id, birthdate, color, gender, name, breed_id) values (12, '11/29/2016', 'Orange', 'Female', 'Nananne', 4);
+
+insert into ANIMALENTITY (id, birthdate, color, gender, name, breed_id) values (13, '2/2/2016', 'Puce', 'Female', 'Tana', 5);
+insert into ANIMALENTITY (id, birthdate, color, gender, name, breed_id) values (14, '6/15/2016', 'Fuscia', 'Male', 'Sutherland', 5);
+insert into ANIMALENTITY (id, birthdate, color, gender, name, breed_id) values (15, '4/30/2017', 'Violet', 'Male', 'Kelsey', 5);
+
+insert into ANIMALENTITY (id, birthdate, color, gender, name, breed_id) values (16, '4/17/2016', 'Red', 'Male', 'Grantham', 6);
+insert into ANIMALENTITY (id, birthdate, color, gender, name, breed_id) values (17, '11/18/2015', 'Red', 'Female', 'Selinda', 6);
+insert into ANIMALENTITY (id, birthdate, color, gender, name, breed_id) values (18, '11/10/2016', 'Maroon', 'Male', 'Orlando', 6);
+
+insert into ANIMALENTITY (id, birthdate, color, gender, name, breed_id) values (19, '12/26/2016', 'Purple', 'Female', 'Kass', 7);
+insert into ANIMALENTITY (id, birthdate, color, gender, name, breed_id) values (20, '12/12/2015', 'Aquamarine', 'Male', 'Rollin', 7);
+insert into ANIMALENTITY (id, birthdate, color, gender, name, breed_id) values (21, '6/7/2017', 'Goldenrod', 'Male', 'Hunfredo', 7);
+
+insert into ANIMALENTITY (id, birthdate, color, gender, name, breed_id) values (22, '11/12/2016', 'Pink', 'Female', 'Cyndy', 8);
+insert into ANIMALENTITY (id, birthdate, color, gender, name, breed_id) values (23, '2/2/2016', 'Purple', 'Male', 'Aldin', 8);
+insert into ANIMALENTITY (id, birthdate, color, gender, name, breed_id) values (24, '7/17/2015', 'Teal', 'Female', 'Rozalie', 8);
+
+insert into ANIMALENTITY (id, birthdate, color, gender, name, breed_id) values (25, '11/17/2016', 'Maroon', 'Female', 'Holly-anne', 9);
+insert into ANIMALENTITY (id, birthdate, color, gender, name, breed_id) values (26, '1/1/2016', 'Indigo', 'Male', 'Morry', 9);
+insert into ANIMALENTITY (id, birthdate, color, gender, name, breed_id) values (27, '6/4/2017', 'Khaki', 'Female', 'Charin', 9);
+
+insert into ANIMALENTITY (id, birthdate, color, gender, name, breed_id) values (28, '4/25/2017', 'Fuscia', 'Female', 'Madelaine', 10);
+insert into ANIMALENTITY (id, birthdate, color, gender, name, breed_id) values (29, '6/9/2017', 'Indigo', 'Female', 'Vanya', 10);
+insert into ANIMALENTITY (id, birthdate, color, gender, name, breed_id) values (30, '4/10/2016', 'Crimson', 'Male', 'Merv', 10);
+
+insert into ANIMALENTITY (id, birthdate, color, gender, name, breed_id) values (31, '2/24/2017', 'Khaki', 'Male', 'Aylmer', 11);
+insert into ANIMALENTITY (id, birthdate, color, gender, name, breed_id) values (32, '10/29/2016', 'Aquamarine', 'Male', 'Onofredo', 11);
+insert into ANIMALENTITY (id, birthdate, color, gender, name, breed_id) values (33, '4/11/2017', 'Turquoise', 'Male', 'Stillmann', 11);
+
+insert into ANIMALENTITY (id, birthdate, color, gender, name, breed_id) values (34, '5/9/2017', 'Crimson', 'Male', 'Ronnie', 12);
+insert into ANIMALENTITY (id, birthdate, color, gender, name, breed_id) values (35, '6/4/2016', 'Crimson', 'Female', 'Wallis', 12);
+insert into ANIMALENTITY (id, birthdate, color, gender, name, breed_id) values (36, '7/27/2016', 'Mauv', 'Female', 'Masha', 12);
---
+++ SQL - PetStore/PhotoAlbumEntity.sql
@@ -0,0 +1,143 @@
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(1, '', 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Kamee01.jpg/245px-Kamee01.jpg', 1);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(2, '', 'http://www.mundoanimalia.com/images/a5/2a/bd/Abisinio/Abisinio_353.jpg', 1);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(3, '', 'https://lh4.googleusercontent.com/-C2DYAygAFqk/Ub15DKduVGI/AAAAAAAABpw/CgZgyp57iC8/s800/Abisinio%25203.jpg', 1);
+
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(4, '', 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Kamee01.jpg/245px-Kamee01.jpg', 2);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(5, '', 'http://www.mundoanimalia.com/images/a5/2a/bd/Abisinio/Abisinio_353.jpg', 2);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(6, '', 'https://lh4.googleusercontent.com/-C2DYAygAFqk/Ub15DKduVGI/AAAAAAAABpw/CgZgyp57iC8/s800/Abisinio%25203.jpg', 2);
+
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(7, '', 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Kamee01.jpg/245px-Kamee01.jpg', 3);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(8, '', 'http://www.mundoanimalia.com/images/a5/2a/bd/Abisinio/Abisinio_353.jpg', 3);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(9, '', 'https://lh4.googleusercontent.com/-C2DYAygAFqk/Ub15DKduVGI/AAAAAAAABpw/CgZgyp57iC8/s800/Abisinio%25203.jpg', 3);
+
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(10, '', 'http://cdn3-www.cattime.com/assets/uploads/gallery/american-curl-cats-and-kittens/american-curl-cats-and-kittens-5.jpg', 4);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(11, '', 'http://cdn3-www.cattime.com/assets/uploads/gallery/american-curl-cats-and-kittens/american-curl-cats-and-kittens-3.jpg', 4);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(12, '', 'http://www.cat-breeds-encyclopedia.com/images/American-curl-cat-stretched-out.jpg', 4);
+
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(13, '', 'http://cdn3-www.cattime.com/assets/uploads/gallery/american-curl-cats-and-kittens/american-curl-cats-and-kittens-5.jpg', 5);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(14, '', 'http://cdn3-www.cattime.com/assets/uploads/gallery/american-curl-cats-and-kittens/american-curl-cats-and-kittens-3.jpg', 5);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(15, '', 'http://www.cat-breeds-encyclopedia.com/images/American-curl-cat-stretched-out.jpg', 5);
+
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(16, '', 'http://cdn3-www.cattime.com/assets/uploads/gallery/american-curl-cats-and-kittens/american-curl-cats-and-kittens-5.jpg', 6);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(17, '', 'http://cdn3-www.cattime.com/assets/uploads/gallery/american-curl-cats-and-kittens/american-curl-cats-and-kittens-3.jpg', 6);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(18, '', 'http://www.cat-breeds-encyclopedia.com/images/American-curl-cat-stretched-out.jpg', 6);
+
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(19, '', 'http://cdn.wikifaunia.com/wp-content/uploads/2012/12/Angora-Turco.jpg', 7);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(20, '', 'http://www.mundogatos.com/Uploads/mundogatos.com/ImagenesGrandes/g-raza-angora.html.jpg', 7);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(21, '', 'https://img.notigatos.es/wp-content/uploads/2011/03/angora_turco.jpg', 7);
+
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(22, '', 'http://cdn.wikifaunia.com/wp-content/uploads/2012/12/Angora-Turco.jpg', 8);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(23, '', 'http://www.mundogatos.com/Uploads/mundogatos.com/ImagenesGrandes/g-raza-angora.html.jpg', 8);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(24, '', 'https://img.notigatos.es/wp-content/uploads/2011/03/angora_turco.jpg', 8);
+
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(25, '', 'http://cdn.wikifaunia.com/wp-content/uploads/2012/12/Angora-Turco.jpg', 9);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(26, '', 'http://www.mundogatos.com/Uploads/mundogatos.com/ImagenesGrandes/g-raza-angora.html.jpg', 9);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(27, '', 'https://img.notigatos.es/wp-content/uploads/2011/03/angora_turco.jpg', 9);
+
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(28, '', 'http://www.mundoanimalia.com/images/b9/af/3d/Barbet/Barbet_5764.jpg', 10);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(29, '', 'http://www.mascotarios.org/wp-content/uploads/2011/04/barbet.jpg', 10);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(30, '', 'http://www.perros.com/content/perros_com/imagenes/thumbs2/1Barbet2.jpg', 10);
+
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(31, '', 'http://www.mundoanimalia.com/images/b9/af/3d/Barbet/Barbet_5764.jpg', 11);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(32, '', 'http://www.mascotarios.org/wp-content/uploads/2011/04/barbet.jpg', 11);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(33, '', 'http://www.perros.com/content/perros_com/imagenes/thumbs2/1Barbet2.jpg', 11);
+
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(34, '', 'http://www.mundoanimalia.com/images/b9/af/3d/Barbet/Barbet_5764.jpg', 12);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(35, '', 'http://www.mascotarios.org/wp-content/uploads/2011/04/barbet.jpg', 12);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(36, '', 'http://www.perros.com/content/perros_com/imagenes/thumbs2/1Barbet2.jpg', 12);
+
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(37, '', 'https://www.dogbreedinfo.com/images25/AmericanBullyUltimateEdgesChampionChopperDog2YearsOld2.jpg', 13);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(38, '', 'https://www.dogbreedinfo.com/images25/AmericanBullyUltimateEdgesChampionChopperDog2YearsOld1.jpg', 13);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(39, '', 'http://americanbullydaily.com/wp-content/uploads/2016/03/Razor-Edge-American-Bully-Picture-370x208.jpg', 13);
+
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(40, '', 'https://www.dogbreedinfo.com/images25/AmericanBullyUltimateEdgesChampionChopperDog2YearsOld2.jpg', 14);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(41, '', 'https://www.dogbreedinfo.com/images25/AmericanBullyUltimateEdgesChampionChopperDog2YearsOld1.jpg', 14);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(42, '', 'http://americanbullydaily.com/wp-content/uploads/2016/03/Razor-Edge-American-Bully-Picture-370x208.jpg', 14);
+
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(43, '', 'https://www.dogbreedinfo.com/images25/AmericanBullyUltimateEdgesChampionChopperDog2YearsOld2.jpg', 15);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(44, '', 'https://www.dogbreedinfo.com/images25/AmericanBullyUltimateEdgesChampionChopperDog2YearsOld1.jpg', 15);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(45, '', 'http://americanbullydaily.com/wp-content/uploads/2016/03/Razor-Edge-American-Bully-Picture-370x208.jpg', 15);
+
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(46, '', 'https://www.pets4homes.co.uk/images/breeds/42/large/547ae7c5ca5d55a9b7a44cc06d93536e.jpg', 16);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(47, '', 'https://s-media-cache-ak0.pinimg.com/originals/04/0e/8b/040e8b886453d9cd5d356291873db8f8.jpg', 16);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(48, '', 'http://www.chophuquochanoi.vn/image_pq/1426091798587_red_american_akita_by_elektra96-d2y2814.jpg', 16);
+
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(49, '', 'https://www.pets4homes.co.uk/images/breeds/42/large/547ae7c5ca5d55a9b7a44cc06d93536e.jpg', 17);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(50, '', 'https://s-media-cache-ak0.pinimg.com/originals/04/0e/8b/040e8b886453d9cd5d356291873db8f8.jpg', 17);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(51, '', 'http://www.chophuquochanoi.vn/image_pq/1426091798587_red_american_akita_by_elektra96-d2y2814.jpg', 17);
+
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(52, '', 'https://www.pets4homes.co.uk/images/breeds/42/large/547ae7c5ca5d55a9b7a44cc06d93536e.jpg', 18);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(53, '', 'https://s-media-cache-ak0.pinimg.com/originals/04/0e/8b/040e8b886453d9cd5d356291873db8f8.jpg', 18);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(54, '', 'http://www.chophuquochanoi.vn/image_pq/1426091798587_red_american_akita_by_elektra96-d2y2814.jpg', 18);
+
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(55, '', 'https://cus21.files.wordpress.com/2008/03/animales-perico.jpg', 19);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(56, '', 'https://www.ecured.cu/images/2/2a/Parrot.jpeg', 19);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(57, '', 'https://lovemattersdotorg.files.wordpress.com/2014/02/parakeet.jpg', 19);
+
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(58, '', 'https://cus21.files.wordpress.com/2008/03/animales-perico.jpg', 20);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(59, '', 'https://www.ecured.cu/images/2/2a/Parrot.jpeg', 20);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(60, '', 'https://lovemattersdotorg.files.wordpress.com/2014/02/parakeet.jpg', 20);
+
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(61, '', 'https://cus21.files.wordpress.com/2008/03/animales-perico.jpg', 21);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(62, '', 'https://www.ecured.cu/images/2/2a/Parrot.jpeg', 21);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(63, '', 'https://lovemattersdotorg.files.wordpress.com/2014/02/parakeet.jpg', 21);
+
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(64, '', 'https://www.hogarmania.com/archivos/201605/canario-aves-668x400x80xX.jpg', 22);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(65, '', 'https://chicosantanna.files.wordpress.com/2015/02/canario-da-terra-rogc3a9riode-castro.jpg?w=401&h=267', 22);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(66, '', 'http://www.animalesenlaweb.com/wp-content/uploads/2015/11/informaci%C3%B3n-sobre-el-canario-2.jpg', 22);
+
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(67, '', 'https://www.hogarmania.com/archivos/201605/canario-aves-668x400x80xX.jpg', 23);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(68, '', 'https://chicosantanna.files.wordpress.com/2015/02/canario-da-terra-rogc3a9riode-castro.jpg?w=401&h=267', 23);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(69, '', 'http://www.animalesenlaweb.com/wp-content/uploads/2015/11/informaci%C3%B3n-sobre-el-canario-2.jpg', 23);
+
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(70, '', 'https://www.hogarmania.com/archivos/201605/canario-aves-668x400x80xX.jpg', 24);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(71, '', 'https://chicosantanna.files.wordpress.com/2015/02/canario-da-terra-rogc3a9riode-castro.jpg?w=401&h=267', 24);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(72, '', 'http://www.animalesenlaweb.com/wp-content/uploads/2015/11/informaci%C3%B3n-sobre-el-canario-2.jpg', 24);
+
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(73, '', 'https://t2.ea.ltmcdn.com/es/razas/9/7/3/img_379_diamante-mandarin_0_600.jpg', 25);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(74, '', 'http://files.aveselpredio.webnode.es/200000161-cb4f7cc492/Diamnte%20Mandarin.jpg', 25);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(75, '', 'http://2.bp.blogspot.com/-VcmfHiyvi7k/TyHR65QrKPI/AAAAAAAAAPw/b9jYk1TNQmk/s1600/Dibujo.jpg', 25);
+
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(76, '', 'https://t2.ea.ltmcdn.com/es/razas/9/7/3/img_379_diamante-mandarin_0_600.jpg', 26);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(77, '', 'http://files.aveselpredio.webnode.es/200000161-cb4f7cc492/Diamnte%20Mandarin.jpg', 26);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(78, '', 'http://2.bp.blogspot.com/-VcmfHiyvi7k/TyHR65QrKPI/AAAAAAAAAPw/b9jYk1TNQmk/s1600/Dibujo.jpg', 26);
+
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(79, '', 'https://t2.ea.ltmcdn.com/es/razas/9/7/3/img_379_diamante-mandarin_0_600.jpg', 27);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(80, '', 'http://files.aveselpredio.webnode.es/200000161-cb4f7cc492/Diamnte%20Mandarin.jpg', 27);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(81, '', 'http://2.bp.blogspot.com/-VcmfHiyvi7k/TyHR65QrKPI/AAAAAAAAAPw/b9jYk1TNQmk/s1600/Dibujo.jpg', 27);
+
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(82, '', 'https://t2.ea.ltmcdn.com/es/images/4/6/5/img_el_huron_como_mascota_20564_600.jpg', 28);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(83, '', 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Ferret_2008.png/220px-Ferret_2008.png', 28);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(84, '', 'https://static.wamiz.fr/images/articles/medium/furet-dans-une-maison.jpg', 28);
+
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(85, '', 'https://t2.ea.ltmcdn.com/es/images/4/6/5/img_el_huron_como_mascota_20564_600.jpg', 29);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(86, '', 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Ferret_2008.png/220px-Ferret_2008.png', 29);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(87, '', 'https://static.wamiz.fr/images/articles/medium/furet-dans-une-maison.jpg', 29);
+
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(88, '', 'https://t2.ea.ltmcdn.com/es/images/4/6/5/img_el_huron_como_mascota_20564_600.jpg', 30);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(89, '', 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Ferret_2008.png/220px-Ferret_2008.png', 30);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(90, '', 'https://static.wamiz.fr/images/articles/medium/furet-dans-une-maison.jpg', 30);
+
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(91, '', 'https://www.anipedia.net/imagenes/hamster-sirio-1.jpg', 31);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(92, '', 'http://www.aquamedor.gr/category_img/1443541426_1.jpeg', 31);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(93, '', 'http://www.cuidatupeludo.com/wp-content/uploads/2017/04/1-13-300x180.jpg', 31);
+
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(94, '', 'https://www.anipedia.net/imagenes/hamster-sirio-1.jpg', 32);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(95, '', 'http://www.aquamedor.gr/category_img/1443541426_1.jpeg', 32);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(96, '', 'http://www.cuidatupeludo.com/wp-content/uploads/2017/04/1-13-300x180.jpg', 32);
+
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(97, '', 'https://www.anipedia.net/imagenes/hamster-sirio-1.jpg', 33);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(98, '', 'http://www.aquamedor.gr/category_img/1443541426_1.jpeg', 33);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(99, '', 'http://www.cuidatupeludo.com/wp-content/uploads/2017/04/1-13-300x180.jpg', 33);
+
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(100, '', 'https://s-media-cache-ak0.pinimg.com/736x/22/30/54/22305418269cbc153015646ab1b2e54d--oaxaca-lemurs.jpg', 34);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(101, '', 'https://cdn-st1.ofpof.com/content/8tmt1wikbm/gallery/_780x780-x3nfxhv5ws.jpg', 34);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(102, '', 'http://images.virgula.uol.com.br/2017/02/perfectly-round-chinchilla-camerons-chinchillas-17-58ad537abe9a4__700-479x479.jpg', 34);
+
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(103, '', 'https://s-media-cache-ak0.pinimg.com/736x/22/30/54/22305418269cbc153015646ab1b2e54d--oaxaca-lemurs.jpg', 35);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(104, '', 'https://cdn-st1.ofpof.com/content/8tmt1wikbm/gallery/_780x780-x3nfxhv5ws.jpg', 35);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(105, '', 'http://images.virgula.uol.com.br/2017/02/perfectly-round-chinchilla-camerons-chinchillas-17-58ad537abe9a4__700-479x479.jpg', 35);
+
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(106, '', 'https://s-media-cache-ak0.pinimg.com/736x/22/30/54/22305418269cbc153015646ab1b2e54d--oaxaca-lemurs.jpg', 36);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(107, '', 'https://cdn-st1.ofpof.com/content/8tmt1wikbm/gallery/_780x780-x3nfxhv5ws.jpg', 36);
+INSERT INTO PHOTOALBUMENTITY (id, name, image, animal_id) VALUES(108, '', 'http://images.virgula.uol.com.br/2017/02/perfectly-round-chinchilla-camerons-chinchillas-17-58ad537abe9a4__700-479x479.jpg', 36);
