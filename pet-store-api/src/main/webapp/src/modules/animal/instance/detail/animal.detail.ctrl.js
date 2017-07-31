b4e0f3971145cba7713e5bbf413aabb8da83e548
eaperador <e.aperador@uniandes.edu.co>
2017-07-31 11:00:46 -0500

    add thumbnail

--- pet-store-api/src/main/webapp/src/modules/animal/instance/detail/animal.detail.ctrl.js
+++ pet-store-api/src/main/webapp/src/modules/animal/instance/detail/animal.detail.ctrl.js
@@ -1,18 +1,14 @@
 /*
 The MIT License (MIT)
-
 Copyright (c) 2015 Los Andes University
-
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
-
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
-
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -25,11 +21,12 @@ SOFTWARE.
 
     var mod = ng.module("animalModule");
 
-    mod.controller("animalDetailCtrl", ['$scope', "$state", "animal","model",
-        function ($scope, $state, animal,model) {
+    mod.controller("animalDetailCtrl", ['$scope', "$state", "animal","model","photoAlbumModel",
+        function ($scope, $state, animal,model, photoAlbumModel) {
             $scope.model = model;
             $scope.currentRecord = animal;
-            $scope.buttons = ['detail','edit'];
+            $scope.photos = animal.getList(photoAlbumModel.url).$object;
+            $scope.photoModel = photoAlbumModel;
             $scope.actions = {
                 create: {
                     displayName: 'Create',
@@ -71,6 +68,39 @@ SOFTWARE.
                     icon: 'link',
                     fn: function () {
                         $state.go('photoAlbumList');
+                    }
+                }
+            };
+            
+            $scope.recordActions = {
+                detail: {
+                    displayName: 'Detail',
+                    icon: 'eye-open',
+                    fn: function (rc) {
+                        $state.go('photoAlbumDetail', {photoAlbumId: rc.id});
+                    },
+                    show: function () {
+                        return true;
+                    }
+                },
+                edit: {
+                    displayName: 'Edit',
+                    icon: 'edit',
+                    fn: function (rc) {
+                        $state.go('photoAlbumEdit', {photoAlbumId: rc.id});
+                    },
+                    show: function () {
+                        return true;
+                    }
+                },
+                delete: {
+                    displayName: 'Delete',
+                    icon: 'minus',
+                    fn: function (rc) {
+                        $state.go('photoAlbumDelete', {photoAlbumId: rc.id});
+                    },
+                    show: function () {
+                        return true;
                     }
                 }
             };
